6478dff5b0ccc80e7aab55e75e4592ef51479e8b
Jacobwkidd <123585537+Jacobwkidd@users.noreply.github.com>
2024-02-06 14:37:52 -0600

    Phone-store I got a few pages done

---
+++ src/app/components/phone-card/phone-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-phone-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './phone-card.component.html',
+  styleUrl: './phone-card.component.css'
+})
+export class PhoneCardComponent {
+
+}
